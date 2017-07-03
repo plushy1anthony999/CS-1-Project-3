6d989a4bc7d179f57b9b648b4171636790b8a58b
anthony ghouri <anthonyghouri@gmail.com>
2017-07-03 20:17:39 +0000

    Added tests in main() for 5 funcs

--- Source.cpp
+++ Source.cpp
@@ -75,16 +75,16 @@ int locationOfMin(const string a[], int n) {
 	return smallestStringPos;
 }
 
-// TODO:
-// ?? Do I put the element after n or at end of array?
 // Moves an element at position "pos" to end of array, and returns its original position
 int moveToEnd(string a[], int n, int pos) {
-	//a[n] = 
+	if (pos > n - 1)
+		return -1;
 	
-	for(int i = pos + 1; i < n; i++) {
-		a[i - 1] = a[i];
+	for(int i = pos; i < n; i++) {
+		a[i] = a[i + 1];
 	}
 	
+	return pos;
 }
 
 // TODO:
@@ -125,47 +125,112 @@ int locateDifference(const string a1[], int n1, const string a2[], int n2) {
 int eliminateDups(string a[], int n) {
 	for(int i = 0; i < n; i++) {
 		if(a[i] == a[i + 1]) {
+			cout << "Duplicate element: " << a[i] << endl;
+
 			int numOfAdjacentDuplicates = 0; // If an element has duplicates next to it, how many duplicates are next to it?
-			
-			int j = i; // Track a second copy of i so that I don't have to modify the for-loop's i
-			while(a[j] == a[j + 1]) { // When a duplicate is found, count number of duplicates
+											 // i.e. if there are 4 "dog" elements, there are 3 duplicates/extras
+	
+			// Counts duplicates from the current i value until a non-duplicate value is found
+			for (int j = i; a[j] == a[j + 1]; j++)
 				numOfAdjacentDuplicates++;
-				j++;
-			}
+
 			
-			// Number of duplicates have been found, so destroy duplicates and
-			// shift elements to the right of the duplicates left
-			for(int k = 0; k < numOfAdjacentDuplicates; k++) {
-				a[i + numOfAdjacentDuplicates + k] = a[i + numOfAdjacentDuplicates + k + 1];
-			}
+
+			cout << "numOfAdjacentDuplicates: " << numOfAdjacentDuplicates << endl;
+			i += numOfAdjacentDuplicates - 1; // Makes for loop just to the element after all the duplicates
+			
+			//// Number of duplicates have been found, so destroy duplicates and
+			//// shift elements to the right of the duplicates remaining
+			//for(int k = 0; k < numOfAdjacentDuplicates; k++) {
+			//	a[i + numOfAdjacentDuplicates + k] = a[i + numOfAdjacentDuplicates + k + 1];
+			//}
 		}
 	}
-	
-	return 
+
+	return 0; // TODO: fix return value
 }
 
 int main() {
-	// moveToBeginning() Test
+	// enumerate() Test	
+	string enumerateArray[] = { "cat", "cat", "pig", "bunny" };
+	assert(enumerate(enumerateArray, 4, "cat") == 2);
+	assert(enumerate(enumerateArray, 1, "cat") == 1);
+	assert(enumerate(enumerateArray, 4, "pig") == 1);
+	assert(enumerate(enumerateArray, 4, "clown") == 0);
+
+	// locate() Test
+	string locateArray[] = { "cat", "cat", "pig", "bunny" };
+	assert(locate(locateArray, 1, "cat") == 0);
+	assert(locate(locateArray, 0, "cat") == -1);
+	assert(locate(locateArray, 0, "duck") == -1);
+	assert(locate(locateArray, 4, "cat") == 0);
+
+	// locateSequence() Test
+	string locateSequenceArray[] = { "cat", "cat", "pig", "pig", "bunny", "duck" };
+	int begin;
+	int end;
+	assert(locateSequence(locateSequenceArray, 6, "cat", begin, end) == true);
+	assert(begin == 0 && end == 1);
+	assert(locateSequence(locateSequenceArray, 1, "cat", begin, end) == true);
+	assert(begin == 0 && end == 0);
+	assert(locateSequence(locateSequenceArray, 3, "pig", begin, end) == true);
+	assert(begin == 2 && end == 2);
+	assert(locateSequence(locateSequenceArray, 3, "dog", begin, end) == false);
+	assert(begin == 2 && end == 2);
+
+	// locationOfMin() Test
+	string locationOfMinArray[] = { "cat", "pig", "dog", "dog", "cat", "duck" };
+	assert(locationOfMin(locationOfMinArray, 6) == 0);
+	string locationOfMinArray2[] = { "duck", "pig", "dog", "dog", "cat", "duck" };
+	assert(locationOfMin(locationOfMinArray2, 6) == 4);
+	string locationOfMinArray3[] = { "cat", "bird", "dog", "dog", "cat", "duck" };
+	assert(locationOfMin(locationOfMinArray3, 6) == 1);
+	string locationOfMinArray4[] = { "pig", "pig", "dog", "dog", "dragon", "duck" };
+	assert(locationOfMin(locationOfMinArray4, 2) == 0);
+
+	// moveToEnd() Test
+	string moveToEndArray[] = { "cat", "pig", "dog", "dog", "cat", "duck" };
+	string originalArray[] = { "cat", "pig", "dog", "dog", "cat", "duck" }; // used for testing, won't be modified by moveToEnd()
+
+	assert(moveToEnd(moveToEndArray, 6, 2) == 2);
+	assert(moveToEndArray[2] == "dog");
+	assert(moveToEndArray[3] == "cat");
+	assert(moveToEndArray[4] == "duck");
+	assert(moveToEndArray[5] == "dog");
+	assert(moveToEnd(moveToEndArray, 6, 5) == 5);
+
+	//assert(moveToEndArray[0] == originalArray[0]);
+	//assert(moveToEndArray[1] == originalArray[1]);
+	//assert(moveToEndArray[2] == originalArray[2]);
+	//assert(moveToEndArray[3] == originalArray[3]);
+	//assert(moveToEndArray[4] == originalArray[4]);
+	//assert(moveToEndArray[5] == originalArray[5]);
+	///*for (int i = 0; i < 6; i++)
+	//	assert(moveToEndArray[i] == originalArray[i]);*/
+
+	//// moveToBeginning() Test
 	// const int NUM_OF_ELEMENTS = 5;
 	// string array[] = {"dog", "cat", "pig", "bunny", "penguin"};
-	
+	//
 	// assert(moveToBeginning(array, NUM_OF_ELEMENTS, 4) == 4);
-	
+	//
 	// for(int i = 0; i < 5; i++) {
 	// 	cout << array[i] << endl;
 	// }
-	
+	//
 	// cout << "All tests succeeded" << endl;
-	
+	//
 	// locateDifference() Test
 	// string array1[] = {"dog", "bunny", "pig", "duck", "bear", "goldfish"};
 	// string array2[] = {"dog", "bunny", "pig", "duck", "bear", "goldfish"};
-	
-	// cout << locateDifference(array1, 6, array2, 6) << endl;
-	string array3[] = {"dog", "dog", "dog", "cat", "bunny", "dog", "dog", "cat", "cat", "bunny"};
-	cout << eliminateDups(array3, 4) << endl;
-	for(int i = 0; i < 10; i++) {
-		cout << array3[i] << endl;
-	}
+	//
+	// cout << locateDifference(array1, 6, array2, 6) << endl;.
+
+	//string array3[] = {"dog", "cat", "dog", "dog", "dog", "dog", "dog", "cat", "cat", "bunny"};
+
+	//cout << eliminateDups(array3, 10) << endl;
+	///*for(int i = 0; i < 10; i++) {
+	//	cout << array3[i] << endl;
+	//}*/
 	
 }
